0b6f2ee1ca04cb2a34518445beacd772bee9753c
AbuMaher <101367311+Abu3Maher@users.noreply.github.com>
2024-12-30 09:26:44 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,15 +23,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="../lib/animate/animate.min.css" rel="stylesheet">
-    <link href="../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="../lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet"/>
+    <link href="{{asset('lib/animate/animate.min.css')}}" rel="stylesheet">
+    <link href="{{asset('lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+    <link href="{{asset('lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css')}}" rel="stylesheet"/>
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
 </head>
 
 <body>
@@ -43,41 +43,6 @@
     </div>
 </div>
 <!-- Spinner End -->
-
-
-<!-- Topbar Start -->
-<div class="container-fluid bg-light p-0 wow fadeIn" data-wow-delay="0.1s">
-    <div class="row gx-0 d-none d-lg-flex">
-        <div class="col-lg-7 px-5 text-start">
-            <div class="h-100 d-inline-flex align-items-center py-3 me-4">
-                <small class="fa fa-map-marker-alt text-primary me-2"></small>
-                <small>123 Street, New York, USA</small>
-            </div>
-            <div class="h-100 d-inline-flex align-items-center py-3">
-                <small class="far fa-clock text-primary me-2"></small>
-                <small>Mon - Fri : 09.00 AM - 09.00 PM</small>
-            </div>
-        </div>
-        <div class="col-lg-5 px-5 text-end">
-            <div class="h-100 d-inline-flex align-items-center py-3 me-4">
-                <small class="fa fa-phone-alt text-primary me-2"></small>
-                <small>+012 345 6789</small>
-            </div>
-            <div class="h-100 d-inline-flex align-items-center">
-                <a class="btn btn-sm-square rounded-circle bg-white text-primary me-1" href=""><i
-                        class="fab fa-facebook-f"></i></a>
-                <a class="btn btn-sm-square rounded-circle bg-white text-primary me-1" href=""><i
-                        class="fab fa-twitter"></i></a>
-                <a class="btn btn-sm-square rounded-circle bg-white text-primary me-1" href=""><i
-                        class="fab fa-linkedin-in"></i></a>
-                <a class="btn btn-sm-square rounded-circle bg-white text-primary me-0" href=""><i
-                        class="fab fa-instagram"></i></a>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Topbar End -->
-
 
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 wow fadeIn" data-wow-delay="0.1s">
@@ -96,7 +61,7 @@
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                 <div class="dropdown-menu rounded-0 rounded-bottom m-0">
                     <a href="feature.html" class="dropdown-item">Feature</a>
-                    <a href="team.html" class="dropdown-item">Our Doctor</a>
+                    <a href="team.blade.php" class="dropdown-item">Our Doctor</a>
                     <a href="appointment.html" class="dropdown-item">Appointment</a>
                     <a href="testimonial.html" class="dropdown-item">Testimonial</a>
                     <a href="404.html" class="dropdown-item">404 Page</a>
@@ -369,7 +334,7 @@
             <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                 <div class="team-item position-relative rounded overflow-hidden">
                     <div class="overflow-hidden">
-                        <img class="img-fluid" src="../img/team-1.jpg" alt="">
+                        <img class="img-fluid" src="{{asset('img/team-1.jpg')}}" alt="">
                     </div>
                     <div class="team-text bg-light text-center p-4">
                         <h5>Doctor Name</h5>
@@ -385,7 +350,7 @@
             <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                 <div class="team-item position-relative rounded overflow-hidden">
                     <div class="overflow-hidden">
-                        <img class="img-fluid" src="../img/team-2.jpg" alt="">
+                        <img class="img-fluid" src="{{asset('img/team-2.jpg')}}" alt="">
                     </div>
                     <div class="team-text bg-light text-center p-4">
                         <h5>Doctor Name</h5>
@@ -639,7 +604,6 @@
 <!-- JavaScript Libraries -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="{{}}"></script>
 <script src="../lib/easing/easing.min.js"></script>
 <script src="../lib/waypoints/waypoints.min.js"></script>
 <script src="../lib/counterup/counterup.min.js"></script>
@@ -648,8 +612,16 @@
 <script src="../lib/tempusdominus/js/moment-timezone.min.js"></script>
 <script src="../lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
 
+{{--<script src=""></script>--}}
+{{--<script src="{{asset('lib/easing/easing.min.js')}}"></script>--}}
+{{--<script src="{{asset('lib/waypoints/waypoints.min.js')}}"></script>--}}
+{{--<script src="{{asset('lib/counterup/counterup.min.js')}}"></script>--}}
+{{--<script src="{{asset('lib/owlcarousel/owl.carousel.min.js')}}"></script>--}}
+{{--<script src="{{asset('lib/tempusdominus/js/moment.min.js)'}}"></script>--}}
+{{--<script src="{{asset('lib/tempusdominus/js/moment-timezone.min.js)'}}"></script>--}}
+{{--<script src="{{asset('lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js)'}}"></script>--}}
 <!-- Template Javascript -->
-<script src="../js/main.js"></script>
+<script src="{{asset('js/main.js')}}"></script>
 </body>
 
 </html>
